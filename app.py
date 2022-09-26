411bbd76ef943ec70720a1c0d9326c4eb425ca61
Donald <59314046+DonaldCapodilupo@users.noreply.github.com>
2022-09-26 08:56:02 -0400

    Added "Go Back" and "Delete Song" logic to the song dashboard generator. Working as intended.

--- app.py
+++ app.py
@@ -9,12 +9,25 @@ def song_Dashboard():
     song_List_Bar_Chart()
     track_list = artist_Retrieve_Song()
     if request.method == "POST" :
-        if request.form['submit_button'] != "Go Back":
+        print(request.form["submit_button"])
+        #Go Back
+        if request.form['submit_button'] == "Go Back":
+            return redirect(url_for("user_Add_Song"))
+        #Show Chords and Lyrics
+        if "Delete" not in  request.form['submit_button']:
             song_information =request.form['submit_button'].replace("[","").replace("]","").replace("'","").split(",")
             song_to_play =  song_information[0] +"/" + song_information[1].lstrip() + ".html"
             return render_template("Song-Dashboard.html", current_tracklist=track_list, song_information=song_to_play)
-    #elif request.form['submit_button'] == "Go Back":
-    #    return redirect(url_for("user_Add_Song"))
+        #Delete Song
+        else:
+            from Backend import delete_Specific_Song_Track_List
+
+            delete_Specific_Song_Track_List(request.form["submit_button"])
+
+            song_List_Bar_Chart()
+            track_list = artist_Retrieve_Song()
+
+            return render_template("Song-Dashboard.html", current_tracklist=track_list)
     if request.method == "GET":
         return render_template("Song-Dashboard.html", current_tracklist=track_list)
 
@@ -25,15 +38,18 @@ def song_Results():
     from Backend import song_List_Bar_Chart, artist_Retrieve_Song
     song_List_Bar_Chart()
     track_list = artist_Retrieve_Song()
+    if request.method == "POST":
+        return redirect(url_for("user_Add_Song"))
     if request.method == "GET":
         return render_template("Song-Vote-Results.html", current_tracklist=track_list)
+
 
 
 @app.route('/', methods=['GET', 'POST'])
 def user_Add_Song():
     if request.method == "POST":
         if request.form['submit_button'] == 'view_results':
-            return redirect(url_for("song_Dashboard"))
+            return redirect(url_for("song_Results"))
         if request.form['submit_button'] == 'submit_song':
             from Backend import read_Entire_Track_List, create_Entry_Track_List
 
